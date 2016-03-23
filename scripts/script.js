1c258d6112cb91544afbc1c42c2ac062b5be8c63
JoedHernandez1995 <joedhernandez95@unitec.edu>
2016-03-23 01:42:24 -0600

    agregando archivos de javascript

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    console.log( "ready!" );
+
+    $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      }
+    );
+    
+    // Initialize collapse button
+    $(".button-collapse").sideNav();
+    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+    //$('.collapsible').collapsible();
+    // Show sideNav
+    $('.button-collapse').sideNav('show');
+    // Hide sideNav
+    $('.button-collapse').sideNav('hide');
+});
