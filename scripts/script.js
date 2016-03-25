a7982668ce28617ba7339845a4dcf207e3244e7d
JoedHernandez1995 <joedhernandez95@unitec.edu>
2016-03-25 01:31:58 -0600

    registro de restaurantes y validacion de formulario

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,56 @@
+function toJSON(formArray){
+  var obj = {};
+  $.each(formArray, function(i, pair){
+    var cObj = obj, pObj, cpName;
+    $.each(pair.name.split("."), function(i, pName){
+      pObj = cObj;
+      cpName = pName;
+      cObj = cObj[pName] ? cObj[pName] : (cObj[pName] = {});
+    });
+    pObj[cpName] = pair.value;
+  });
+  return JSON.stringify(obj);
+}
+
 $(document).ready(function() {
-    console.log( "ready!" );
+    console.log( "ready!" ); //Test jquery is ready
+
+    /*Submit Registration Form*/
+    $("#submitRegistration").submit(function(event){
+      /*Get data from password fields and assign values to variables*/
+      var registrationPassword = $("#restaurant_registrationPassword").val();
+      var confirmRegistrationPassword = $("#restaurant_confirmRegistrationPassword").val();
+
+      /*Validate if password length is equal or greater than 8 characters */
+      if(registrationPassword.length < 8){
+        alert("La longitud de la contrasena debe ser mayor o igual a 8 caracteres");
+        event.preventDefault(); //Prevent Form Submission
+      }else{
+        /*Validate if password and password confirmation fields match*/
+        if(registrationPassword != confirmRegistrationPassword){
+          alert("Las contrasenas no son iguales, vuelva a introducirlas");
+          event.preventDefault(); //Prevent Form Submission
+        }else{
+          /*Sumbit Form*/
+          var registrationFormArray = $("#submitRegistration").serializeArray(); //Serialize all the data in the form
+          var registrationJSONData = toJSON(registrationFormArray); //Convert form data to JSON formData
+          $.ajax({
+            type: "POST",
+            url: "http://feedmeserver.herokuapp.com/restaurante_create",
+            data: registrationJSONData,
+            contentType: "application/json",
+            dataType: 'json'
+          })
+          .done(function(data, textStatus, jqXHR){
+            console.log("Ajax completed: " + data);
+          })
+          .fail(function(jqXHR, textStatus, errorThrown){
+            console.log("Ajax problem: " + textStatus + ". " + errorThrown);
+          });
+          event.preventDefault();
+        }
+      }
+    }); //End Registration Form Submit 
 
     /*Hacer la accion del select al hacer un cambio en el status de la orden*/
     /*
