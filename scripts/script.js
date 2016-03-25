9a57f9751e4a96d78742ae6c751189c245490bf3
JoedHernandez1995 <joedhernandez95@unitec.edu>
2016-03-24 22:38:15 -0600

    interfaz de login listo

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
     console.log( "ready!" );
 
+    /*Hacer la accion del select al hacer un cambio en el status de la orden*/
+    /*
+    $("#orderStatus").change(function(){
+      var text = $("option:selected",this).text();
+      console.log(text);
+    });*/
+
+    /*Materialize code*/
+
     $('.button-collapse').sideNav({
       menuWidth: 300, // Default is 240
       edge: 'right', // Choose the horizontal origin
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
       }
     );
-    
+
+    $('select').material_select();
     // Initialize collapse button
     $(".button-collapse").sideNav();
     // Initialize collapsible (uncomment the line below if you use the dropdown variation)
